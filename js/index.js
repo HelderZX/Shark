9757d29aba066bf23dcba27261cb768c04feb37c
Helder <helderandrade15p@gmail.com>
2023-10-07 18:31:46 +0100

    Animations and Shark images

--- js/index.js
+++ js/index.js
@@ -9,71 +9,70 @@ const data = {
         "Estima-se que eles possam viver até 70 anos.",
         "São uma espécie ameaçada devido à pesca indiscriminada para a obtenção de barbatanas.",
         "O tubarão-baleia é um peixe ovovivíparo, o que significa que os embriões se desenvolvem dentro do corpo da mãe antes de nascerem.",
-        "São geralmente solitários, mas podem se reunir em grupos em áreas ricas em alimento."
-      ],
-      "Image": "img/sharks/Whale_Shark.svg"
-    },
-    "Tubarão-Martelo": {
-      "Curiosities": [
-        "Os tubarões-martelo são conhecidos por suas cabeças distintas em forma de 'T'.",
-        "Essa forma única da cabeça ajuda na detecção de presas e proporciona uma melhor visão.",
-        "Existem várias espécies de tubarões-martelo, incluindo o grande tubarão-martelo, o tubarão-martelo liso e o tubarão-martelo-panã.",
-        "Eles são conhecidos por serem excelentes caçadores e nadadores ágeis.",
-        "Os tubarões-martelo vivem em uma variedade de habitats, desde águas costeiras até o oceano aberto.",
-        "São frequentemente encontrados em águas tropicais e temperadas.",
-        "Esses tubarões podem formar grandes escolas durante a migração.",
-        "São predadores oportunistas que se alimentam de peixes, raias, lulas e outros invertebrados marinhos.",
-        "Algumas espécies de tubarões-martelo estão ameaçadas devido à pesca comercial."
+        "São geralmente solitários, mas podem se reunir em grupos em áreas ricas em alimento.",
+        "Eles são classificados como uma espécie vulnerável devido à pressão crescente de atividades humanas.",
       ],
       "Image": "img/sharks/Whale_Shark.svg"
     },
     "Tubarão-Tigre": {
       "Curiosities": [
-        "O tubarão-tigre é conhecido por suas listras escuras e barriga amarelada.",
-        "São encontrados em águas tropicais e subtropicais em todo o mundo.",
-        "Possuem um comportamento noturno e são frequentemente ativos à noite.",
-        "São conhecidos por serem vorazes comedores e se alimentam de uma variedade de presas, incluindo tartarugas marinhas, aves marinhas e outros peixes.",
-        "São uma das espécies de tubarões mais agressivas em relação aos seres humanos.",
-        "Podem crescer até 5 metros de comprimento.",
-        "São vivíparos, com as fêmeas dando à luz filhotes vivos.",
-        "Os tubarões-tigre são conhecidos por serem resistentes e podem sobreviver em diferentes tipos de ambientes marinhos."
+        "Eles recebem o nome 'tubarão-tigre' devido às listras escuras em forma de tigre que aparecem quando são jovens. À medida que envelhecem, essas listras tendem a desaparecer.",
+        "Esses tubarões podem crescer até tamanhos consideráveis, geralmente atingindo entre 3 e 4 metros de comprimento. Alguns indivíduos até podem exceder 5 metros.",
+        "Esses tubarões podem crescer até tamanhos consideráveis, geralmente atingindo entre 3 e 4 metros de comprimento. Alguns indivíduos até podem exceder 5 metros.",
+        "Eles são conhecidos por serem caçadores oportunísticos, o que significa que se alimentam de uma variedade de presas, incluindo peixes, tartarugas, aves marinhas e até mesmo outros tubarões.",
+        "Os tubarões-tigre são frequentemente ativos à noite, caçando sob a cobertura da escuridão.",
+        "São conhecidos por seu comportamento agressivo em relação a outros tubarões, frequentemente lutando por comida ou território.",
+        "Os tubarões-tigre têm uma expectativa de vida relativamente longa, vivendo até cerca de 30 anos em condições ideais.",
+        "São tubarões vivíparos, o que significa que seus filhotes se desenvolvem dentro do corpo da mãe e nascem vivos. As fêmeas podem dar à luz a ninhadas de até 80 filhotes de cada vez.",
+        "Como predadores marinhos, os tubarões-tigre desempenham um papel importante no equilíbrio dos ecossistemas marinhos, controlando populações de presas e ajudando a manter a saúde dos oceanos.",
+        "Os tubarões-tigre têm uma mordida poderosa e são capazes de esmagar ossos com seus dentes afiados. Sua força de mordida é uma das mais poderosas entre os tubarões.",
       ],
-      "Image": "img/sharks/Whale_Shark.svg"
-    },
-    "Tubarão-Limão": {
-      "Curiosities": [
-        "Os tubarões-limão têm uma coloração amarelo-brilhante e são frequentemente encontrados em águas rasas do Caribe.",
-        "São conhecidos por suas grandes nadadeiras dorsais e peitorais.",
-        "Geralmente preferem habitats costeiros, mas podem migrar para águas mais profundas.",
-        "Possuem um comportamento mais pacífico em relação aos humanos, mas podem se tornar agressivos se provocados.",
-        "São vivíparos e dão à luz ninhadas de filhotes que podem variar em tamanho.",
-        "São excelentes nadadores e podem viajar longas distâncias em busca de alimento.",
-        "Sua dieta inclui uma variedade de peixes, raias e crustáceos.",
-        "A pesca comercial e a degradação do habitat são ameaças para a espécie."
-      ],
-      "Image": "img/sharks/Whale_Shark.svg"
+      "Image": "img/sharks/Tiger_Shark.svg"
     },
     "Tubarão-Mako": {
       "Curiosities": [
-        "O tubarão-mako é um dos tubarões mais velozes, podendo atingir velocidades de até 70 km/h.",
-        "São frequentemente encontrados em águas oceânicas profundas, mas também podem se aproximar da costa.",
-        "Têm um corpo esguio e uma coloração azul metálica distintiva.",
-        "São predadores ágeis que se alimentam principalmente de peixes, incluindo atuns e bonitos.",
-        "São vivíparos, dando à luz filhotes já desenvolvidos.",
-        "A carne do tubarão-mako é valorizada e é alvo de pesca comercial, o que levou a preocupações de sobrepesca."
+        "O tubarão-mako é conhecido por ser um dos tubarões mais rápidos do oceano, sendo capaz de atingir velocidades de até 70 km/h (43 mph).",
+        "Eles são encontrados em oceanos tropicais e temperados de todo o mundo, mas são mais comuns em águas oceânicas profundas.",
+        "São predadores ágeis que se alimentam principalmente de peixes, lulas e ocasionalmente de outros animais marinhos.",
+        "Os tubarões-mako são conhecidos por realizar saltos espetaculares fora da água, às vezes atingindo alturas consideráveis.",
+        "Assim como o tubarão-tigre, os tubarões-mako são vivíparos, o que significa que seus filhotes se desenvolvem dentro do corpo da mãe e nascem vivos.",
+        "O tubarão-mako-comum (Isurus oxyrinchus) é listado como uma espécie vulnerável devido à pesca excessiva, enquanto o tubarão-mako-anão (Isurus paucus) é classificado como quase ameaçado.",
+        "Os tubarões-mako têm uma expectativa de vida relativamente longa, que pode chegar a até 30 anos em condições ideais.",
+        "Os tubarões-mako são conhecidos por suas longas migrações sazonais, viajando longas distâncias em busca de alimento e águas mais quentes.",
+        "O tubarão-mako-anão (Isurus paucus) é uma espécie menor de tubarão-mako em comparação com o mako-comum (Isurus oxyrinchus).",
+        "Os tubarões-mako são conhecidos por serem alvos populares de pesca esportiva devido à sua força e resistência durante a luta.",
       ],
-      "Image": "img/sharks/Whale_Shark.svg"
+      "Image": "img/sharks/Mako_Shark.svg"
     },
-    "Tubarão-Zebra": {
+    "Tubarão-frade": {
       "Curiosities": [
-        "O tubarão-zebra é conhecido por suas listras escuras em um fundo claro, semelhantes a uma zebra.",
-        "São encontrados em recifes de coral e áreas costeiras em todo o Indo-Pacífico.",
-        "São mais ativos durante o dia e se alimentam de presas como caranguejos, moluscos e pequenos peixes.",
-        "São uma espécie relativamente dócil e raramente representam uma ameaça para os seres humanos.",
-        "São ovovivíparos, com as fêmeas dando à luz filhotes que medem cerca de 30 centímetros de comprimento.",
-        "A degradação dos recifes de coral representa uma ameaça significativa para o habitat do tubarão-zebra."
+        "O tubarão-frade é conhecido por sua aparência única, com um corpo robusto, barbatanas peitorais largas e uma cabeça achatada.",
+        "Eles são frequentemente encontrados em águas tropicais e subtropicais, incluindo recifes de coral e áreas costeiras rasas.",
+        "O tubarão-frade é um caçador noturno que se alimenta principalmente de invertebrados marinhos, como caranguejos, moluscos e crustáceos.",
+        "Possui uma cauda grossa que é usada para se apoiar no leito do oceano enquanto descansa ou se alimenta.",
+        "Eles possuem uma maneira única de respirar, usando uma técnica chamada respiração buconasal, onde sugam água pela boca e a expulsam pelas narinas.",
+        "O tubarão-frade é conhecido por seu comportamento tranquilo e raramente é agressivo em relação aos humanos.",
+        "As fêmeas dão à luz filhotes vivos, mas eles se desenvolvem em ovos dentro do corpo da mãe antes do nascimento.",
+        "A expectativa de vida dos tubarões-frade pode chegar a 25 anos ou mais.",
+        "São mais ativos à noite, saindo de seus esconderijos para caçar e se alimentar.",
+        "Os tubarões-frade desempenham um papel importante na manutenção do equilíbrio ecológico dos recifes de coral, controlando as populações de invertebrados.",
       ],
-      "Image": "img/sharks/Whale_Shark.svg"
+      "Image": "img/sharks/Basking_Shark.svg"
+    },
+    "Tubarão-Raposo": {
+      "Curiosities": [
+        "Os tubarões-raposa são encontrados em oceanos tropicais e temperados em todo o mundo.",
+        "Eles têm um corpo longo e esguio, com uma coloração azul-acinzentada no dorso e uma barriga branca.",
+        "Uma das características mais distintas dos tubarões-raposa são suas barbatanas caudais extremamente longas, que podem chegar a um terço do comprimento total do corpo.",
+        "São caçadores especializados em peixes pelágicos, como atuns e cavalas.",
+        "Os tubarões-raposa são conhecidos por realizar migrações sazonais, movendo-se para águas mais quentes no inverno e mais frias no verão.",
+        "Assim como outros tubarões, eles são vivíparos, com as fêmeas dando à luz filhotes vivos após um longo período de gestação.",
+        "As fêmeas do tubarão-raposa têm uma das gestações mais longas entre os tubarões, podendo durar mais de 2 anos, e geralmente dão à luz apenas uma ninhada a cada 2 ou 3 anos.",
+        "Os tubarões-raposa são alvos de pesca comercial devido à sua carne e barbatanas, o que levou a preocupações sobre sua conservação em algumas regiões.",
+        "Eles desempenham um papel importante na regulação das populações de peixes pelágicos, ajudando a manter o equilíbrio nos ecossistemas marinhos.",
+        "O status de conservação do tubarão-raposa varia em diferentes partes de sua distribuição, com algumas populações consideradas vulneráveis devido à pesca intensiva."
+      ],
+      "Image": "img/sharks/Common_Thresher_Shark.svg"
     }
 }
 
@@ -104,5 +103,8 @@ change();
 let button = document.getElementById('btn-next');
 
 button.addEventListener("click", () => {
-    change();
+    $('.content').addClass('animate__backOutUp');
+    $('.content').on('animationend', () => {
+        $('.content').removeClass('animate__backOutUp').addClass('animate__backInUp');
+    })
 });
